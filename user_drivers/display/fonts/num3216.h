67445771d9fdd8ff66aa8b37bf622deb85d6a898
Zach <zach20722303@outlook.com>
2026-08-05 22:40:48 +0800

    feat: temp font 3216

--- user_drivers/display/fonts/num3216.h
+++ user_drivers/display/fonts/num3216.h
@@ -3,12 +3,16 @@
 
 #include "user_def.h"
 
-typedef enum {
+typedef enum
+{
     FONT_NUM3216_1 = 1, // MS Serif (Like Song font, print-like)
     FONT_NUM3216_2,     // MS Sans Serif (Like Hei font, print-like)
     FONT_NUM3216_3,     // MS Gothic (Like Kai font, print-like)
     FONT_NUM3216_4,     // MS Mincho (Like Fangsong font, print-like)
-    FONT_COLON3216,     // Colon (for time display)
+    FONT_COLON3216,     // Colon
+    FONT_DOT3216,       // Dot
+    FONT_DASH3216,      // Dash
+    FONT_BLANK3216,     // Blank
     FONT_NUM3216_MAX
 } font_num3216_index_t;
 
--- user_drivers/display/pages/Page_Temp.c
+++ user_drivers/display/pages/Page_Temp.c
@@ -10,16 +10,40 @@
  */
 #include "Page_Temp.h"
 #include "temp.h"
+#include "app_clock.h"
 
 void Page_Temp_Subtrate(void)
 {
-    oled_show_string(OLED_ROW(1), OLED_COL(1), "Temperature");
-    oled_show_icon16(OLED_ROW(3), OLED_COL(5), Icon16_TEMPERATURE);
-    oled_show_string(OLED_ROW(3), OLED_COL(7), "  . `C");
+    oled_show_icon16(OLED_ROW(1), OLED_COL(1), Icon16_TEMPERATURE);
+    oled_show_string(OLED_ROW(1), OLED_COL(4) - 4, "Temp");
+    oled_show_char(OLED_ROW(1), OLED_COL(14), ':');
+    oled_show_string(4, OLED_COL(12) + 4, "`C");
 }
 
 void Page_Temp_Task(void)
 {
-    oled_show_int(OLED_ROW(3), OLED_COL(7), t_int, 2, false);
-    oled_show_int(OLED_ROW(3), OLED_COL(10), t_deci_1, 1, true);
+    s8 t_int_tmp = t_int;
+    u8 t_deci_tmp = t_deci_1;
+    oled_show_int(OLED_ROW(1), OLED_COL(12), Calendar.hour, 2, true);
+    oled_show_int(OLED_ROW(1), OLED_COL(15), Calendar.min, 2, true);
+    if (t_int_tmp < 0) {
+        t_int_tmp = -t_int_tmp;
+        if (t_int_tmp >= 10) {
+            oled_show_num3216(OLED_ROW(3)-1, OLED_COL(3), 0, FONT_DASH3216);
+            oled_show_num3216(OLED_ROW(3)-1, OLED_COL(5), t_int_tmp / 10, FONT_NUM3216_1);
+        } else {
+            oled_show_num3216(OLED_ROW(3)-1, OLED_COL(3), 0, FONT_BLANK3216);
+            oled_show_num3216(OLED_ROW(3)-1, OLED_COL(5), 0, FONT_DASH3216);
+        }
+    } else {
+        oled_show_num3216(OLED_ROW(3)-1, OLED_COL(3), 0, FONT_BLANK3216);
+        if (t_int_tmp >= 10) {
+            oled_show_num3216(OLED_ROW(3)-1, OLED_COL(5), t_int_tmp / 10, FONT_NUM3216_1);
+        } else {
+            oled_show_num3216(OLED_ROW(3)-1, OLED_COL(5), 0, FONT_BLANK3216);
+        }
+    }
+    oled_show_num3216(OLED_ROW(3)-1, OLED_COL(7), t_int_tmp % 10, FONT_NUM3216_1);
+    oled_show_num3216(OLED_ROW(3)-1, OLED_COL(9), 0, FONT_DOT3216);
+    oled_show_num3216(OLED_ROW(3)-1, OLED_COL(10), t_deci_tmp, FONT_NUM3216_1);
 }
